8346e579fcf6547eb6ce1d627469ef1f65fc9fb7
Shankar S <112791993+Shanky43@users.noreply.github.com>
2023-05-12 10:26:01 +0530

    first code

--- frontend/src/Pages/SignIn.tsx
+++ frontend/src/Pages/SignIn.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
-// import loginVbanner from "../Assets/loginpagebanner.mp4"
+import HomeBanner from "../Assets/homeBanner.mp4";
+import SIgnIn from "../Assets/signin.mp4"
+import fitnessB from "../Assets/fitnessB-transformed.png"
+import { Link } from 'react-router-dom';
 const SignIn = () => {
   return (
     <div>
-      <div className='w-full h-screen flex items-start'>
-        <div className='relative w-1/2 h-full flex flex-col'>
-          {/* <video src={loginVbanner}   ></video> */}
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro ducimus at possimus explicabo temporibus ab voluptatibus ea incidunt provident, iste qui sapiente dolor doloribus dolorem impedit, iusto iure similique quisquam.
-          Accusantium repudiandae quae aspernatur consequatur doloremque labore esse aliquam accusamus neque repellat earum a architecto ea, possimus omnis vero. Fugiat facilis consectetur recusandae similique ipsa. Blanditiis provident minima vel sequi!
-          Quo nostrum inventore similique molestias natus nam! Dolorem ratione consequatur eligendi iure pariatur! Saepe esse, quia, ad eaque est a vel voluptate explicabo fugit facilis, excepturi incidunt dolore enim similique.
-          Quis eos aliquam repellendus corrupti, inventore deleniti dolore debitis magni perspiciatis ducimus, quas velit nostrum ad explicabo placeat sapiente laborum nobis voluptas id commodi? Ut debitis possimus sint impedit dolorem.
-          Ea labore beatae fugit officiis, ipsum esse officia veritatis temporibus ab fugiat. Fuga hic vel, excepturi molestias neque odio debitis officiis quos amet earum nam rerum, autem eveniet beatae perferendis?
-
+      <div className='w-full h-full flex items-start'>
+        <div className='relative w-full h-full flex flex-col object-cover ' style={{ backgroundColor: 'black' }}>
+          <video
+            autoPlay
+            loop
+            controls={false}
+            muted
+            src={SIgnIn}
+            className='opacity-30'
+          // style={{
+          //   // opacity: 0.5,
+          //   backgroundColor: 'rgba(0,0,0,0.5)'
+          // }}
+          >
+          </video>
+          <div className='absolute top-[5%] text-white  left-[5%] flex font-bold'>
+            <Link to="/">
+              <img src={fitnessB} alt="fitnessb"
+                style={{
+                  filter: 'brightness(0%) invert(100%)',
+                  width: "200px"
+                }}
+              />
+            </Link>
+          </div>
         </div>
         <div></div>
       </div>
