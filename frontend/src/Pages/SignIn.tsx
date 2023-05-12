1567c9eb4fde34f9542ec25b732f530e9db4e129
Shankar S <112791993+Shanky43@users.noreply.github.com>
2023-05-12 11:36:13 +0530

    first code

--- frontend/src/Pages/SignIn.tsx
+++ frontend/src/Pages/SignIn.tsx
@@ -44,7 +44,7 @@ const SignIn = () => {
   };
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-
+    console.log(name + "-" + value)
     switch (name) {
       case 'email':
         setEmail(value);
@@ -71,21 +71,22 @@ const SignIn = () => {
           {/* form start here */}
           <div className="flex flex-1 flex-col justify-center lg:px-8 w-screen">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-              <form className="space-y-6" action="#" method="POST">
+              <form className="space-y-6" onSubmit={handleSubmit} >
                 <div>
                   <label htmlFor="email" className="block text-sm text-left font-medium leading-6 text-white">
                     Email address
                   </label>
                   <div className="mt-2">
                     <input
-                      id="email"
                       name="email"
                       type="email"
-                      autoComplete="email"
+                      value={email}
+                      // autoComplete="email"
                       required
                       onChange={handleInputChange}
                       className="block w-full rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-white placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6"
                     />
+                     <p className='text-sm text-[#f2305a]'>{formErrors.password && <span className="error">{formErrors.password}</span>}</p> 
                   </div>
                 </div>
 
@@ -102,14 +103,15 @@ const SignIn = () => {
                   </div>
                   <div className="mt-2">
                     <input
-                      id="password"
                       name="password"
                       type="password"
+                      value={password}
                       onChange={handleInputChange}
-                      autoComplete="current-password"
+                      // autoComplete="current-password"
                       required
                       className="block w-full rounded-md border-0 py-2 px-3  text-gray-900 shadow-sm ring-1 ring-inset ring-white placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6"
                     />
+                     <p className='text-sm text-[#f2305a]'>{formErrors.password && <span className="error">{formErrors.password}</span>}</p>
                   </div>
                 </div>
 
@@ -139,7 +141,7 @@ const SignIn = () => {
               src={fitnessB}
               alt="fitnessb"
               style={{
-                width: "300px",
+                width: "250px",
               }}
             />
           </Link>
