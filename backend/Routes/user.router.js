925799cc9a24a0621b023dd5702683cfb56b46db
Shankar S <112791993+Shanky43@users.noreply.github.com>
2023-05-12 21:14:32 +0530

    fitnessB backend

--- backend/Routes/user.router.js
+++ backend/Routes/user.router.js
@@ -70,7 +70,7 @@ userRouter.post("/login", async (req, res) => {
 userRouter.get("/", async (req, res) => {
   try {
     const users = await userModel.find();
-    res.status(200).json();
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: error });
   }
