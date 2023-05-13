5979f77ff984816436dd4b880d22b22b359927a7
SurajV2000 <sv107719@gmail.com>
2023-05-13 17:14:16 +0530

    BMI ANALYSUS

---
+++ frontend/src/CHarts/Charts.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { Doughnut } from 'react-chartjs-2'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: ["Exercise", "Walking", "Rest"],
+  datasets: [
+    {
+      label: "My BMI analysis",
+      data: [300, 50, 100],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+const config = {
+  type: "doughnut",
+  data: data,
+};
+const Charts = () => {
+  return (
+    <Doughnut data={data} />
+  )
+}
+
+export default Charts
